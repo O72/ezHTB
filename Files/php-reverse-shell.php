a49704fb67744163ddc258603b93194849a40741
O72 <aljaloudo@gmail.com>
2020-04-09 02:43:09 -0400

    php-reverse-shell

--- Files/php-reverse-shell.php
+++ Files/php-reverse-shell.php
@@ -46,8 +46,8 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '127.0.0.1';  // CHANGE THIS
-$port = 1234;       // CHANGE THIS
+$ip = '127.0.0.1';  // CHANGE this
+$port = 1234;       // CHANGE this
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
